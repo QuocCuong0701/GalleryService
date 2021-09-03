66979af16d30f2f5c7b28bffbe7cfb232eccb454
QuocCuong <cuongpq@bssd.vn>
2021-09-03 22:34:22 +0700

    cron

--- src/main/java/com/project/upload/utils/ScheduleUtil.java
+++ src/main/java/com/project/upload/utils/ScheduleUtil.java
@@ -12,7 +12,7 @@ public class ScheduleUtil {
     @Autowired
     FileService fileService;
 
-    @Scheduled(cron = "0 33 22 ? * *")
+    @Scheduled(cron = "0 35 22 ? * *")
     public void updateIfFileNotExist() {
         fileService.updateIfFileNotExist();
     }
